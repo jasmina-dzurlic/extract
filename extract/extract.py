6558c8b76789a4f6352283fd76dc9c0106cf2226
aparnac25 <aparnac25@gmail.com>
2021-03-06 22:57:12 -0500

    changed project name

---
+++ extract/extract.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+A function to extract GPS coordinates from a PDF table and outputting a CSV file with coordinates and plotting to a map
+"""
+
+# First step in creating program is to extract the relevant infromation from a PDF table 
+
+# import tabula_py
+# import paandas as pd 
+
