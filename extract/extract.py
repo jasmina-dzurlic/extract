4172d5c5096caf4e8c9fad5993e88532dfedb2a4
aparnac25 <aparnac25@gmail.com>
2021-03-15 14:01:45 -0400

    working on fixing path

--- extract/extract.py
+++ extract/extract.py
@@ -6,6 +6,18 @@ A function to extract GPS coordinates from a PDF table and outputting a CSV file
 
 # First step in creating program is to extract the relevant infromation from a PDF table 
 
-# import tabula_py
-# import paandas as pd 
+import tabula
+import pandas as pd 
+import os
 
+# create a pdf path to the documents folder where you will have your PDF documents
+ 
+relative_path = '/documents/Murphy2017.pdf'
+current_dir = os.getcwd()
+pdf_path = os.path.join(current_dir, relative_path)
+
+print(pdf_path)
+
+# create a table variable and read all the pages and extract all the tables from the PDF 
+
+# all_tables = tabula.read_pdf(pdf_path, pages = "all", multiple_tables = True)
