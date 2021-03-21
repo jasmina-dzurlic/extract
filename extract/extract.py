2ec2e6c6d7410fc0dcc2246723b214f5a7c89393
aparnac25 <aparnac25@gmail.com>
2021-03-21 19:07:21 -0400

    edited extract file and added extract function

--- extract/extract.py
+++ extract/extract.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-A function to extract GPS coordinates from a PDF table and outputting a CSV file with coordinates and plotting to a map
+A function to extract GPS coordinates from a PDF and outputting a CSV file with coordinates and plotting to a map
 """
 
 # First step in creating program is to extract the relevant infromation from a PDF 
@@ -11,15 +11,24 @@ import pandas as pd
 import os
 
 # create a pdf path to the documents folder where you will have your PDF documents
-
-pdf_path = os.path.realpath("../project/documents/MurphyRTL2017.pdf")
-
+# pdf_path = os.path.realpath("../project/documents/MurphyRTL2017.pdf")
 # opening up PDF with tika parser
-
-parsed_pdf = parser.from_file(pdf_path)
-
+# parsed_pdf = parser.from_file(pdf_path)
 # saving content of PDF
+# data = parsed_pdf['content']
 
-data = parsed_pdf['content']
+def extract(pdf_path):
+    """This function will extract the content from a PDF file and retrun it
+    when given a path"""
+    # opening up PDF with tika parser
+    parsed_pdf = parser.from_file(pdf_path)
+    # saving content of PDF
+    data = parsed_pdf['content']
+    return data 
 
-print(data)
+# asks user to import the path to pdf that they want extract to get the content from (remeber to have .pdf at end of file --add
+# that to the help section)
+pdf_path = input("Please input pdf path \n").lower()
+
+if __name__ == "__main__":
+    extract(pdf_path)
