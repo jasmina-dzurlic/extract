c59bde470485ee0788537c211ab912557214fa81
aparnac25 <aparnac25@gmail.com>
2021-03-21 19:05:59 -0400

    editing setup.py but not working

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python
+"""
+Install extract package. To install locally use:
+	pip install -e .
+"""
+
+from setuptools import setup
+
+setup(
+    name="extract",
+    version="0.0.1",
+    entry_points={
+        'console_scripts': ['extract = extract:extract']
+    }
+)
