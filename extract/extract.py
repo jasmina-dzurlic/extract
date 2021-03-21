a9935f2ee9564a257fc87fdaf116fa4ab6accd48
aparnac25 <aparnac25@gmail.com>
2021-03-21 16:29:07 -0400

    edited code to read PDF with tika

--- extract/extract.py
+++ extract/extract.py
@@ -4,21 +4,22 @@
 A function to extract GPS coordinates from a PDF table and outputting a CSV file with coordinates and plotting to a map
 """
 
-# First step in creating program is to extract the relevant infromation from a PDF table 
+# First step in creating program is to extract the relevant infromation from a PDF 
 
-import tabula
+from tika import parser
 import pandas as pd 
 import os
 
 # create a pdf path to the documents folder where you will have your PDF documents
- 
-#pdf_path =os.relativepath(/project/documents/MurphyRTL2017.pdf)
 
-pdf_path = "https://link.springer.com/content/pdf/10.1007/s10764-017-9967-8.pdf"
+pdf_path = os.path.realpath("../project/documents/MurphyRTL2017.pdf")
 
-# create a table variable and read all the pages and extract all the tables from the PDF 
+# opening up PDF with tika parser
 
-all_tables = tabula.read_pdf(pdf_path, pages = "all", stream = True)
+parsed_pdf = parser.from_file(pdf_path)
 
-all_tables.head()
-# read the first table with coordiantes into a dataframe
+# saving content of PDF
+
+data = parsed_pdf['content']
+
+print(data)
