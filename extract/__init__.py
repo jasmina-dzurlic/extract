6ce9b0a2226e227a0b7061b78e97cc3a90dbcee6
aparnac25 <aparnac25@gmail.com>
2021-03-21 21:33:29 -0400

    editing code but still not working

--- extract/__init__.py
+++ extract/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
-init file for extract package"""
+init file for extract package
+"""
 
-__version__ = "0.0.1"
 
-from .extract import extract
+from extract.extract import extract
