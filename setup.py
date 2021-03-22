2e2b8e96acf0b7bc8b7ca634934e25b6eb405da9
aparnac25 <aparnac25@gmail.com>
2021-03-21 21:33:46 -0400

    edited code but still not working

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """
-Install extract package. To install locally use:
-	pip install -e .
+Install extract package. To install locally use: pip install -e .
 """
 
 from setuptools import setup
@@ -10,5 +9,10 @@ from setuptools import setup
 setup(
     name="extract",
     version="0.0.1",
-    description="package to read pdf for coordinates and plot to map"
+    author="Aparna Chandrashekar",
+    author_email="achandrashekar@gradcenter.cuny.edu",
+    description="program to read PDF and extract coordinates to plot onto map'",
+    install_requires = ["pandas", "tika"], 
+    classifiers=["Programming Language :: Python :: 3"],
+    scripts= ['extract/extract.py']
 )
